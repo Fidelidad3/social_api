f7796f15cabb76b1815ec3132a41e93f3aeb0254
viacheslav.panevskyi <viacheslav.panevskyi@symmetrics.de>
2015-08-06 23:10:49 +0300

    Relation service tests

--- tests/app/Models/Services/RelationsTest.php
+++ tests/app/Models/Services/RelationsTest.php
@@ -37,4 +37,42 @@ class RelationsTest extends PHPUnit_Framework_TestCase
         $relationService = new Relations();
         $relationService->rejectFriendRequest($relationship);
     }
+
+    public function testApproveFriendRequest()
+    {
+        $startNode = m::mock('Everyman\Neo4j\Node');
+        $endNode = m::mock('Everyman\Neo4j\Node');
+
+        $directFriendRelationship = m::mock('Everyman\Neo4j\Relationship');
+        $directFriendRelationship->shouldReceive('save')
+            ->once();
+
+        $indirectFriendRelationship = m::mock('Everyman\Neo4j\Relationship');
+        $indirectFriendRelationship->shouldReceive('save')
+            ->once();
+
+        $startNode->shouldReceive('relateTo')
+            ->once()
+            ->with($endNode, 'FRIENDS')
+            ->andReturn($directFriendRelationship);
+
+        $endNode->shouldReceive('relateTo')
+            ->once()
+            ->with($startNode, 'FRIENDS')
+            ->andReturn($indirectFriendRelationship);
+
+        $relationship = m::mock('Everyman\Neo4j\Relationship');
+        $relationship->shouldReceive('getStartNode')
+            ->once()
+            ->andReturn($startNode);
+        $relationship->shouldReceive('getEndNode')
+            ->once()
+            ->andReturn($endNode);
+        $relationship->shouldReceive('delete')
+            ->once();
+
+        $relationService = new Relations();
+        $relationService->approveFriendRequest($relationship);
+    }
+
 }
